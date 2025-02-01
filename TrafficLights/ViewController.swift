61a23d35e6e93b1ca280c19a58abdb34cffc6a14
MaDoG <aliasgraphics@icloud.com>
2025-02-01 11:53:43 +0300

    Refacturing ViewController

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,60 +9,47 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet private var greenLightView: UIView!
-    @IBOutlet private var orangeLightView: UIView!
-    @IBOutlet private var redLightView: UIView!
-    @IBOutlet private var startButton: UIButton!
+    @IBOutlet private var greenView: UIView!
+    @IBOutlet private var orangeView: UIView!
+    @IBOutlet private var redView: UIView!
+    
+    @IBOutlet private var switchLightButton: UIButton!
     
     private let trafficLightsStage = (lightIsOff: 0.3, lightIsOn: 1.0)
-    private var trafficLightsSwitch = TrafficLightColorSwitch.red
+    private var trafficLightsColorSwitch = TrafficLightColorSwitch.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startButton.layer.cornerRadius = 10
+        switchLightButton.layer.cornerRadius = 10
         
-        greenLightView.alpha = trafficLightsStage.lightIsOff
-        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        greenView.alpha = trafficLightsStage.lightIsOff
+        orangeView.alpha = trafficLightsStage.lightIsOff
+        redView.alpha = trafficLightsStage.lightIsOff
         
-        orangeLightView.alpha = trafficLightsStage.lightIsOff
-        orangeLightView.layer.cornerRadius = orangeLightView.frame.width / 2
-        
-        redLightView.alpha = trafficLightsStage.lightIsOff
-        redLightView.layer.cornerRadius = redLightView.frame.width / 2
-        
-//        redLightView.alpha = trafficLightsSetting.lightOff
-//        redLightView.layer.cornerRadius = redLightView.frame.size.height/2
-//        orangeLightView.alpha = trafficLightsSetting.lightOff
-//        orangeLightView.layer.cornerRadius = orangeLightView.frame.size.height/2
-//        greenLightView.alpha = trafficLightsSetting.lightOff
-//        greenLightView.layer.cornerRadius = greenLightView.frame.size.height/2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        orangeView.layer.cornerRadius = orangeView.frame.width / 2
+        redView.layer.cornerRadius = redView.frame.width / 2
     }
 
-    @IBAction func startButtonDidTapped() {
-//        if !startButton.currentTitle?.contains("Next") {
-            
+    @IBAction func switchLightButtonDidTapped() {
+        if switchLightButton.currentTitle != "Next" {
+            switchLightButton.setTitle("NEXT", for: .normal)
         }
-//        startButton.setTitle("NEXT", for: .normal)
         
-//        switch trafficLightsStage {
-//        case 1:
-//            redLightView.alpha = trafficLightsSetting.lightOn
-//            greenLightView.alpha = trafficLightsSetting.lightOff
-//            trafficLightsStage = 2
-//        case 2:
-//            redLightView.alpha = trafficLightsSetting.lightOff
-//            orangeLightView.alpha = trafficLightsSetting.lightOn
-//            trafficLightsStage = 3
-//        case 3:
-//            orangeLightView.alpha = trafficLightsSetting.lightOff
-//            greenLightView.alpha = trafficLightsSetting.lightOn
-//            trafficLightsStage = 1
-//        default:
-//            redLightView.alpha = trafficLightsSetting.lightOff
-//            orangeLightView.alpha = trafficLightsSetting.lightOff
-//            greenLightView.alpha = trafficLightsSetting.lightOff
-//            
-//        }
+        switch trafficLightsColorSwitch {
+        case .red:
+            redView.alpha = trafficLightsStage.lightIsOn
+            greenView.alpha = trafficLightsStage.lightIsOff
+            trafficLightsColorSwitch = .yellow
+        case .yellow:
+            redView.alpha = trafficLightsStage.lightIsOff
+            orangeView.alpha = trafficLightsStage.lightIsOn
+            trafficLightsColorSwitch = .green
+        case .green:
+            orangeView.alpha = trafficLightsStage.lightIsOff
+            greenView.alpha = trafficLightsStage.lightIsOn
+            trafficLightsColorSwitch = .red
+        }
         
     }
 }
